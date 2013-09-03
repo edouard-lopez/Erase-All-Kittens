f3df01ec0bf9574998d455d4ff61add0fb6d75f9
Joe Dytrych <me@jdytrych.co.uk>
2013-09-03 11:30:53 +0100

    Added kitchen sink and hovering

---
+++ levels/4/config.js
@@ -0,0 +1,12 @@
+{
+  "name": "Kitchen Sink",
+  "width": 720,
+  "height": 640,
+  "borders": "all",
+  "background": "white",
+  "player": {
+    "x": 0,
+    "y": -320
+  },
+  "target": "<div style='background: red; display: inline; position: absolute; top: 0; right: 0;'>Target</div>"
+}
